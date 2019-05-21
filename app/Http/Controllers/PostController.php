5dbdbafd95e7dca707b89938a625cfcf154025fb
ZIN MYO <overmidnight12@gmail.com>
2019-05-21 11:22:41 -0700

    first commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function __construct()
+    {
+        return $this->middleware('auth');
+    }
+
+    public function index()
+	{
+    $posts = Post::all();
+
+    return view('index', compact('posts'));
+	}
+
+	public function show($id)
+	{
+    $post = Post::find($id);
+
+    return view('show', compact('post'));
+	}
+
+    public function create()
+    {
+        return view('post');
+    }
+
+	public function store(Request $request)
+    {
+        $post =  new Post;
+        $post->title = $request->get('title');
+        $post->body = $request->get('body');
+
+        $post->save();
+
+        return redirect('posts');
+
+    }
+}
